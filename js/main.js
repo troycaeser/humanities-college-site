36cdd3de01749f3d911a385a7b1f523394536792
Troy Huang <troycaeser@hotmail.com>
2014-05-29 20:21:17 +1000

    fixed overflow issue with big_nav navigation bar

--- js/main.js
+++ js/main.js
@@ -11,41 +11,6 @@ $(document).ready(function() {
 		}
 	});
 
-	// 	var cbpAnimatedHeader = (function() {
-	 
-	//     var docElem = document.documentElement,
-	//         header = document.querySelector( '.cbp-af-header' ),
-	//         didScroll = false,
-	//         changeHeaderOn = 300;
-	 
-	//     function init() {
-	//         window.addEventListener( 'scroll', function( event ) {
-	//             if( !didScroll ) {
-	//                 didScroll = true;
-	//                 setTimeout( scrollPage, 250 );
-	//             }
-	//         }, false );
-	//     }
-	 
-	//     function scrollPage() {
-	//         var sy = scrollY();
-	//         if ( sy >= changeHeaderOn ) {
-	//             classie.add( header, 'cbp-af-header-shrink' );
-	//         }
-	//         else {
-	//             classie.remove( header, 'cbp-af-header-shrink' );
-	//         }
-	//         didScroll = false;
-	//     }
-	 
-	//     function scrollY() {
-	//         return window.pageYOffset || docElem.scrollTop;
-	//     }
-	 
-	//     init();
-	 
-	// })();
-
 	var menu = new cbpTooltipMenu( document.getElementById( 'cbp-tm-menu' ) );
 
 	var support = { animations : Modernizr.cssanimations },
