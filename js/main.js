a8ef0715aa3644ed364327d6eeb8c8c787fd0b0b
Troy Huang <troycaeser@hotmail.com>
2014-06-03 22:35:11 +1000

    some scrollspy functionality and menu height fix

--- js/main.js
+++ js/main.js
@@ -2,14 +2,34 @@ $(document).ready(function() {
 
 	$(window).scroll(function(){
 		var current_scroll_position = $(this).scrollTop();
+		var section_one = $('#sec1').offset().top;
+		var section_two = $('#sec2').offset().top;
+		var section_three = $('#sec3').offset().top;
 
 		if(current_scroll_position > 30){
 			$('.big_nav').addClass('big_nav_shrink');
+			$('.hero-main').addClass('hero-main-shrink')
 		}
 		else if(current_scroll_position < 30){
 			$('.big_nav').removeClass('big_nav_shrink');
+			$('.hero-main').removeClass('hero-main-shrink');
 			$('li').removeClass('active');
 		}
+
+		var stuff = $('.post').text();
+		// alert(stuff);
+
+		// var pos = $('#breadcrumb').offset();
+		// alert(pos);
+		$('.post').each(function(){
+			if(current_scroll_position >= $(this).offset().top && current_scroll_position <= $(this).next().offset().top)
+			{
+				// $('#breadcrumb').html('stuff > more >> ' + $(this).html());
+				$('#breadcrumb').html($(this).offset().top);
+
+				return; //break the loop
+			}
+		});
 	});
 
 	var menu = new cbpTooltipMenu( document.getElementById( 'cbp-tm-menu' ) );
